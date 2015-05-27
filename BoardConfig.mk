f6596f05c9040a233a495e8885da51674fb6165e
Jesse <whoshotjr2006@gmail.com>
2015-05-27 02:44:46 -0400

    fixed sms, working on mms

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,8 @@
 USE_CAMERA_STUB := false
 TARGET_NO_BOOTLOADER := true
 
+TARGET_SPECIFIC_HEADER_PATH := device/lge/x5/include
+
 # Platform
 TARGET_ARCH := arm
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
